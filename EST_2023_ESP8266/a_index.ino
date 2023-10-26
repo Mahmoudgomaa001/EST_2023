f1c39eaddbafac1718dc5e4a275baef9c8952e1d
Mahmoudgomaa001 <117765723+Mahmoudgomaa001@users.noreply.github.com>
2023-10-26 16:38:50 +0300

    Update a_index.ino

--- EST_2023_ESP8266/a_index.ino
+++ EST_2023_ESP8266/a_index.ino
@@ -47,7 +47,7 @@ const char index_html[] PROGMEM = R"rawliteral(<!DOCTYPE HTML><html>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="40" height="40"><path d="M288 32c0 17.7 14.3 32 32 32h32c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H352c53 0 96-43 96-96s-43-96-96-96H320c-17.7 0-32 14.3-32 32zm64 352c0 17.7 14.3 32 32 32h32c53 0 96-43 96-96s-43-96-96-96H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H384c-17.7 0-32 14.3-32 32zM128 512h32c53 0 96-43 96-96s-43-96-96-96H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H160c17.7 0 32 14.3 32 32s-14.3 32-32 32H128c-17.7 0-32 14.3-32 32s14.3 32 32 32z" fill="#ffd43b"/></svg>
                 <span class="dht-labels">Concentration</span>
                 <span id="dust">%DUST%</span>
-                <sup class="units">ug/m3</sup>
+                <sup class="units"></sup>
               </p>
             
 
